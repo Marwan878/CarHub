c669029c6a64ea4ff70a654183d4e1f2e60f9874
unknown <marwanmohammad432@gmail.com>
2024-10-12 15:59:25 +0300

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "cdn.imagin.studio"
+            }
+        ]
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    }
+};
+
+export default nextConfig;
